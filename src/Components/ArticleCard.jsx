9e6a7865f9b684fa31bb8d90039858a259d8f9b9
Stepharinaa <stephanielplee@gmail.com>
2025-04-01 10:48:33 +0100

    correct comment to be comment_count, as that matches api

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 function ArticleCard({ article }) {
   return (
-    <article>
+    <article className="article-card">
       <h2>
         <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
       </h2>
       <p>By {article.author}</p>
       <p>Topic: {article.topic}</p>
       <p>Votes: {article.votes}</p>
-      <p>Comments: {article.comments}</p>
+      <p>Comments: {article.comment_count}</p>
     </article>
   );
 }
