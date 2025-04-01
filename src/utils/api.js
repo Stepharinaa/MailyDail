be321650d37c98b79a9c955f3120dbef139f138a
Stepharinaa <stephanielplee@gmail.com>
2025-04-01 10:05:29 +0100

    adjust function name to fetch and ensure error is thrown

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,13 +4,14 @@ const api = axios.create({
   baseURL: "https://stephs-northcoders-news.onrender.com/api",
 });
 
-const getArticles = () => {
+const fetchArticles = () => {
   return api
     .get("/articles")
     .then(({ data }) => data.articles)
     .catch((error) => {
       console.error("Error fetching articles:", error);
+      throw error;
     });
 };
 
-export { getArticles };
+export { fetchArticles };
