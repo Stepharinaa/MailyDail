b12c21ba8cf416a23dcdfbe5aa51fa0ea7374fca
Stepharinaa <stephanielplee@gmail.com>
2025-04-01 13:42:35 +0100

    correct welcome message for maily dail

--- src/Components/WelcomeMessage.jsx
+++ src/Components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 function WelcomeMessage() {
     return (
       <section className="welcome-message">
-        <h1>Welcome to NC News!</h1>
+        <h1>Welcome to Maily Dail!</h1>
         <p>Your go-to source for the latest articles on a variety of topics. Stay informed, engage with the community, and enjoy insightful discussions.</p>
       </section>
     );
