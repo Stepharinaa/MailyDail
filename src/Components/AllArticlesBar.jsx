6bf1a313761092989b22e0b99b1b6ea37abd8b04
Stepharinaa <stephanielplee@gmail.com>
2025-04-01 14:22:54 +0100

    adjust original search bar to utilise fetch topics function

--- src/Components/AllArticlesBar.jsx
+++ src/Components/AllArticlesBar.jsx
@@ -1,32 +1,30 @@
 import { useState} from 'react'
+import { fetchArticles } from '../utils/api'
 
-function AllArticlesBar({ onSortChange, onTopicChange, topics}) {
-    const [sortBy, setSortBy] = useState("created_at")
-    const [topic, setTopic] = useState("")
+function AllArticlesBar({ setSortBy, setTopic}) {
+const [topics, setTopics] = useState([])
 
-    const handleSortChange = (event) => {
-        const newSortBy = event.target.value
-        setSortBy(newSortBy)
-        onSortChange(newSortBy)
-    }
-
-    const handleTopicChange = (event) => {
-        const newTopic = event.target.value;
-        setTopic(newTopic);
-        onTopicChange(newTopic);
-    }
+useEffect(() => {
+    fetchTopics()
+      .then((data) => {
+        setTopics(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching topics:", error);
+      });
+  }, []);
 
     return (
         <div className="all-articles-bar">
           <label htmlFor="sort-by">Sort by:</label>
-          <select id="sort-by" value={sortBy} onChange={handleSortChange}>
+          <select id="sort-by" onChange={(element) => setSortBy(element.target.value)}>
             <option value="created_at">Date</option>
             <option value="votes">Votes</option>
             <option value="comment_count">Comments</option>
           </select>
     
           <label htmlFor="topic-filter">Filter by Topic:</label>
-          <select id="topic-filter" value={topic} onChange={handleTopicChange}>
+          <select id="topic-filter" onChange={(element) => setTopic(element.target.value)}>
             <option value="">All Topics</option>
             {topics.map((topic) => (
               <option key={topic.slug} value={topic.slug}>
