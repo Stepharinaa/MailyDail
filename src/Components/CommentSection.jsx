bc91cbc0a81ee838f464db646e3fa55035315620
Stepharinaa <stephanielplee@gmail.com>
2025-04-01 16:45:33 +0100

    add comment form into comment section

--- src/Components/CommentSection.jsx
+++ src/Components/CommentSection.jsx
@@ -1,9 +1,11 @@
 import {useState, useEffect} from "react"
-import { fetchCommentsByArticleID } from "../utils/api"
+import { fetchCommentsByArticleID, postCommentByArticleID } from "../utils/api"
 
 function CommentSection({articleID}) {
     const [comments, setComments] = useState([])
+    const [newComment, setNewComment] = useState("")
     const [isLoading, setIsLoading] = useState(true)
+    const [isPosting, setIsPosting] = useState(false)
 
     useEffect(() => {
         fetchCommentsByArticleID()
@@ -17,8 +19,36 @@ function CommentSection({articleID}) {
         })
     } ,[articleID])
 
+    const handleCommentSubmit = (event) => {
+        event.preventDefault()
+
+        setIsPosting(true)
+        postCommentByArticleID(articleID, { username: "grumpy19", body: newComment})
+        .then((comment) => {
+            setComments((previousComments) => [comment, ...previousComments])
+            setNewComment("")
+        })
+        .catch((error) => {
+            console.error("Error posting comment", error)
+        })
+        .finally(() => {
+            setIsPosting(false)
+        })
+    }
+
 return (
     <section className="comment-section">
+        <form onSubmit={handleCommentSubmit} className="comment-form">
+        <textarea
+          value={newComment}
+          onChange={(element) => setNewComment(element.target.value)}
+          placeholder="Add comment here..."
+          required
+        />
+        <button type="submit" disabled={isPosting}>
+          {isPosting ? "Posting..." : "Add Comment"}
+        </button>
+        </form>
         <h3>Comments</h3>
         <ul className="comment-list">
         {comments.length > 0 ? (
