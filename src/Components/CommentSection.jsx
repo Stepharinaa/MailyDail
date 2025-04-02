c8cb6e37d19100bb04814345828814d8de617747
Stepharinaa <stephanielplee@gmail.com>
2025-04-02 16:31:17 +0100

    add delete button to comment section file

--- src/Components/CommentSection.jsx
+++ src/Components/CommentSection.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom"
-import { fetchCommentsByArticleID, postCommentByArticleID } from "../utils/api"
+import { fetchCommentsByArticleID, postCommentByArticleID, deleteCommentByCommentID } from "../utils/api"
 
 function CommentSection() {
   const {article_id} = useParams()
@@ -8,8 +8,10 @@ function CommentSection() {
     const [newComment, setNewComment] = useState("")
     const [isLoading, setIsLoading] = useState(true)
     const [isPosting, setIsPosting] = useState(false)
+    const [isDeleting, setIsDeleting] = useState({})
     const [confirmationMessage, setConfirmationMessage] = useState(null)
     const [error, setError] = useState(null)
+    const currentUser = "grumpy19"
 
     useEffect(() => {
       setIsLoading(true)
@@ -29,7 +31,7 @@ function CommentSection() {
         if (!newComment.trim()) return;
 
         setIsPosting(true)
-        postCommentByArticleID(article_id, { username: "Cool New User", body: newComment})
+        postCommentByArticleID(article_id, { username: currentUser, body: newComment})
         .then((comment) => {
             setComments((previousComments) => [comment, ...previousComments])
             setNewComment("")
@@ -42,6 +44,25 @@ function CommentSection() {
         .finally(() => {
             setIsPosting(false)
         })
+    }
+    
+    const handleDeleteComment = (comment_id) => {
+      setIsDeleting((previousComments) => ({ ...previousComments, [comment_id]: true}))
+
+      const filteredOutComments = comments.filter(comment => comment.comment_id !== comment_id)
+      setComments(filteredOutComments)
+
+      deleteCommentByCommentID(comment_id)
+      .then(() => {
+        setConfirmationMessage("Comment deleted successfully")
+      }).catch((error) => {
+        console.error("Error deleting comment", error)
+        setError("Your comment could not be deleted. Please try again!")
+        setComments((previousComments) => [...previousComments, filteredOutComments])
+      })
+      .finally(() => {
+        setIsDeleting((previousComments) => ({...previousComments, [comment_id] : false}))
+      })
     }
 
 return (
@@ -70,6 +91,16 @@ return (
               </div>
                <p>{comment.body}</p>
                <p>Votes: {comment.votes}</p>
+
+               {comment.author === currentUser && (
+                <button
+                  className="delete-button"
+                  onClick={() => handleDeleteComment(comment.comment_id)}
+                  disabled={isDeleting[comment.comment_id]}
+                >
+                  {isDeleting[comment.comment_id] ? "Deleting..." : "Delete"}
+                </button>
+              )}  
 
             </li>
           ))
