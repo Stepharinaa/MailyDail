b084a3e37bc13e47264b2917a4366df6cee01da0
Stepharinaa <stephanielplee@gmail.com>
2025-04-02 11:57:12 +0100

    add optimistic rendering for votes

--- src/Components/ArticleVotesButton.jsx
+++ src/Components/ArticleVotesButton.jsx
@@ -1,13 +1,20 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import { updateVotesOnArticle } from "../utils/api"
 
 function ArticleVotesButton({articleID, currentVotes}) {
     const [votes, setVotes] = useState(currentVotes)
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setVotes(currentVotes);
+    }, [currentVotes]);
 
     const handleVote = (voteChange) => {
+        setVotes((previousVotes) => previousVotes + voteChange)
+        setError(null)
         setIsLoading(true)
-        console.log("Article ID:", articleID);
+
         updateVotesOnArticle(articleID, voteChange)
           .then((updatedArticle) => {
             setVotes(updatedArticle.votes)
@@ -15,6 +22,7 @@ function ArticleVotesButton({articleID, currentVotes}) {
           })
           .catch((error) => {
             console.error("Vote update failed:", error)
+            setError("Your vote was not successful. Please try again!")
             setIsLoading(false)
           })
           if (isLoading) return <p>Loading votes...</p>
@@ -25,6 +33,7 @@ function ArticleVotesButton({articleID, currentVotes}) {
             <p>Votes: {votes} </p>
             <button className="votes-box" onClick={() => handleVote(1)}>👍</button>
             <button className="votes-box" onClick={() => handleVote(-1)}>👎</button>
+            {error && <p id="error-message">{error}</p>}
         </div>
     )
 }
