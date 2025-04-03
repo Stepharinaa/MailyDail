081c9cd0806e90ce535803f90aee191b946ed4fe
Stepharinaa <stephanielplee@gmail.com>
2025-04-03 15:14:48 +0100

    add order query and state into allarticlespage

--- src/Pages/AllArticlesPage.jsx
+++ src/Pages/AllArticlesPage.jsx
@@ -8,12 +8,12 @@ function AllArticlesPage(){
     const [articles, setArticles] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [sortBy, setSortBy] = useState("created_at");
+    const [order, setOrder] = useState("DESC")
     const [topic, setTopic] = useState("");
 
     useEffect(() => {
-        console.log("Fetching articles with:", `sortBy=${sortBy}`, `topic=${topic}`);
         setIsLoading(true)
-        fetchArticles(sortBy, topic)
+        fetchArticles(sortBy, order, topic)
         .then((data) => {
             setArticles(data)
             setIsLoading(false)
@@ -22,14 +22,14 @@ function AllArticlesPage(){
             console.error("Error fetching articles:", error)
             setIsLoading(false)
         })
-    }, [sortBy, topic])
+    }, [sortBy, order, topic])
 
 
 return (
     <main>
         <h1>All Articles</h1>
         <AllArticlesBar setTopic={setTopic} />
-        <SortByBox onSortChange={setSortBy} />
+        <SortByBox onSortChange={setSortBy} onOrderChange={setOrder}/>
         {isLoading ? <p>Loading articles...</p> : <ArticleList articles={articles} />}
     </main>
 );
