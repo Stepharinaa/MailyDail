4eb814740dd23fb471f21af4f35bce295947ffda
Stepharinaa <stephanielplee@gmail.com>
2025-04-03 15:23:01 +0100

    add orderby option to sortbybox component

--- src/Components/SortByBox.jsx
+++ src/Components/SortByBox.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
-function SortByBox({ onSortChange }) {
+function SortByBox({ onSortChange, onOrderChange }) {
   const [sortBy, setSortBy] = useState("created_at");
+  const [order, setOrder] = useState("DESC")
 
   const handleSortChange = (event) => {
     const selectedSort = event.target.value;
@@ -9,16 +10,28 @@ function SortByBox({ onSortChange }) {
     onSortChange(selectedSort);
   };
 
+  const handleOrderChange = (event) => {
+    const selectedOrder = event.target.value;
+    setOrder(selectedOrder);
+    onOrderChange(selectedOrder);
+  };
+
   return (
     <div className="sort-by-bar">
       <label htmlFor="sort-by">Sort by:</label>
       <select id="sort-by" value={sortBy} onChange={handleSortChange}>
-        <option value="created_at">Most Recent</option>
-        <option value="votes">Most Votes</option>
-        <option value="comment_count">Most Comments</option>
+        <option value="created_at">Date</option>
+        <option value="votes">Votes</option>
+        <option value="comment_count">Comments</option>
+      </select>
+
+      <label htmlFor="order-by">Order by:</label>
+      <select id="order-by" value={order} onChange={handleOrderChange}>
+      <option value="DESC">Descending</option>
+      <option value="ASC">Ascending</option>
       </select>
     </div>
-  );
+  )
 }
 
 export default SortByBox;
