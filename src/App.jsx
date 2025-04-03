79f15481d93a1e92925366d52b53f2f904e319fe
Stepharinaa <stephanielplee@gmail.com>
2025-04-03 16:35:28 +0100

    add invalid path to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
           <Route path="/articles/:article_id" element={<SingleArticlePage />} />
           <Route path="/topics" element={<AllTopicsPage />} />
           <Route path="/topics/:slug" element={<ArticlesByTopicPage />} />
+          <Route path="*" element={<InvalidPathPage />} />
         </Routes>
       </div>
   )
