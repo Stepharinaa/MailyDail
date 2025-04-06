75f7451633218b82532902b11ce4851c6ef36b3d
Stepharinaa <stephanielplee@gmail.com>
2025-04-06 15:46:53 +0100

    add new route/page to app.jsx, which will be form to add topic

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ function App() {
           <Route path="/articles/:article_id" element={<SingleArticlePage />} />
           <Route path="/topics" element={<AllTopicsPage />} />
           <Route path="/topics/:slug" element={<ArticlesByTopicPage />} />
+          <Route path="/topics/create" element={<CreateTopicPage />}/>
           <Route path="*" element={<InvalidPathPage />} />
         </Routes>
       </div>
