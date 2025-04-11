f27436130c7cc011f5d1f09d7844705fd81c9d6d
Stepharinaa <stephanielplee@gmail.com>
2025-04-11 10:33:20 +0100

    adjust comments card to hyperlink to relevant article

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
-import articlePlaceHolderImage from "../assets/article-placeholder-image.png"
+import articlePlaceHolderImage from "../assets/article-placeholder-image.png";
 import timeAgo from "../utils/formatTimeToNow";
 
 function ArticleCard({ article }) {
   return (
     <article className="article-card">
-      <img src={article.article_img_url || articlePlaceHolderImage} alt={article.title} className="article-img" />
+      <img
+        src={article.article_img_url || articlePlaceHolderImage}
+        alt={article.title}
+        className="article-img"
+      />
       <div className="article-content">
         <h2 className="article-title">
           <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
         </h2>
-          <p className="article-meta">Topic: {article.topic}</p>
-          <p className="article-meta">Created: {timeAgo(article.created_at)}</p>
+        <p className="article-meta">Topic: {article.topic}</p>
+        <p className="article-meta">Created: {timeAgo(article.created_at)}</p>
         <span className="article-author">Written by: {article.author}</span>
-          <div className="comments-box">
-          <span className="comments-value">✍️ {article.comment_count} Comments</span>
+        <div className="comments-box">
+          <span className="comments-value">
+            <Link to={`/articles/${article.article_id}`}>
+              ✍️ {article.comment_count} Comments
+            </Link>
+          </span>
         </div>
       </div>
     </article>
