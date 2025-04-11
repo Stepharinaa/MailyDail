13ce8a1398b59e33f0b7fadcbcb03ad48500c955
Stepharinaa <stephanielplee@gmail.com>
2025-04-12 00:51:38 +0100

    change red and green arrows to default white arrows on votes button

--- src/Components/ArticleVotesButton.jsx
+++ src/Components/ArticleVotesButton.jsx
@@ -1,45 +1,49 @@
-import {useState, useEffect} from "react"
-import { updateVotesOnArticle } from "../utils/api"
-import upvoteButton from "../assets/green-arrow.png"
-import downvoteButton from "../assets/red-arrow.png"
+import { useState, useEffect } from "react";
+import { updateVotesOnArticle } from "../utils/api";
+import { TiArrowDownOutline } from "react-icons/ti";
+import { TiArrowUpOutline } from "react-icons/ti";
 
-function ArticleVotesButton({articleID, currentVotes}) {
-    const [votes, setVotes] = useState(currentVotes)
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
+function ArticleVotesButton({ articleID, currentVotes }) {
+  const [votes, setVotes] = useState(currentVotes);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setIsLoading(true)
-        setVotes(currentVotes);
-    }, [currentVotes]);
+  useEffect(() => {
+    setIsLoading(true);
+    setVotes(currentVotes);
+  }, [currentVotes]);
 
-    const handleVote = (voteChange) => {
-        setVotes((previousVotes) => previousVotes + voteChange)
-        setError(null)
-        setIsLoading(true)
+  const handleVote = (voteChange) => {
+    setVotes((previousVotes) => previousVotes + voteChange);
+    setError(null);
+    setIsLoading(true);
 
-        updateVotesOnArticle(articleID, voteChange)
-          .then((updatedArticle) => {
-            setVotes(updatedArticle.votes)
-            setIsLoading(false)
-          })
-          .catch((error) => {
-            console.error("Vote update failed:", error)
-            setError("Your vote was not successful. Please try again!")
-            setIsLoading(false)
-          })
-        
-          if (isLoading) return <p>Loading votes...</p>
-        }
+    updateVotesOnArticle(articleID, voteChange)
+      .then((updatedArticle) => {
+        setVotes(updatedArticle.votes);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Vote update failed:", error);
+        setError("Your vote was not successful. Please try again!");
+        setIsLoading(false);
+      });
 
-    return(
-        <div className="vote-container"> 
-            <button className="vote-button" onClick={() => handleVote(1)}><img src={upvoteButton} alt="Upvote Arrow" className="vote-icon" /></button>
-            <p className="vote-count">{votes}</p>
-            <button className="vote-button" onClick={() => handleVote(-1)}><img src={downvoteButton} alt="Downvote Arrow" className="vote-icon" /></button>
-            {error && <p className="error-message">{error}</p>}
-        </div>
-    )
+    if (isLoading) return <p>Loading votes...</p>;
+  };
+
+  return (
+    <div className="vote-container">
+      <button className="vote-button" onClick={() => handleVote(1)}>
+        <TiArrowUpOutline className="vote-icon" />
+      </button>
+      <p className="vote-count">{votes}</p>
+      <button className="vote-button" onClick={() => handleVote(-1)}>
+        <TiArrowDownOutline className="vote-icon" />
+      </button>
+      {error && <p className="error-message">{error}</p>}
+    </div>
+  );
 }
 
-export default ArticleVotesButton
+export default ArticleVotesButton;
--- src/Components/CommentVotesButton.jsx
+++ src/Components/CommentVotesButton.jsx
@@ -1,45 +1,49 @@
-import { useState, useEffect } from "react"
-import { updateVotesByCommentID } from "../utils/api"
-import LoadingAnimation from "./LoadingAnimation"
-import upvoteButton from "../assets/green-arrow.png"
-import downvoteButton from "../assets/red-arrow.png"
+import { useState, useEffect } from "react";
+import { updateVotesByCommentID } from "../utils/api";
+import LoadingAnimation from "./LoadingAnimation";
+import { TiArrowDownOutline } from "react-icons/ti";
+import { TiArrowUpOutline } from "react-icons/ti";
 
-function CommentVotesButton({commentID, currentVotes}) {
-    const [votes, setVotes] = useState(currentVotes)
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
+function CommentVotesButton({ commentID, currentVotes }) {
+  const [votes, setVotes] = useState(currentVotes);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setIsLoading(true)
-        setVotes(currentVotes);
-    }, [currentVotes])
+  useEffect(() => {
+    setIsLoading(true);
+    setVotes(currentVotes);
+  }, [currentVotes]);
 
-    const handleVote = (voteChange) => {
-        setVotes((previousVotes) => previousVotes + voteChange)
-        setError(null)
-        setIsLoading(true)
+  const handleVote = (voteChange) => {
+    setVotes((previousVotes) => previousVotes + voteChange);
+    setError(null);
+    setIsLoading(true);
 
-        updateVotesByCommentID(commentID, voteChange)
-          .then((updatedComment) => {
-            setVotes(updatedComment.votes)
-            setIsLoading(false)
-          })
-          .catch((error) => {
-            console.error("Vote update failed:", error)
-            setError("Your vote was not successful. Please try again!")
-            setIsLoading(false)
-          })
-          if (isLoading) return <LoadingAnimation />;
-          if (error) return <p className="error-message">{error}</p>
-        }
+    updateVotesByCommentID(commentID, voteChange)
+      .then((updatedComment) => {
+        setVotes(updatedComment.votes);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Vote update failed:", error);
+        setError("Your vote was not successful. Please try again!");
+        setIsLoading(false);
+      });
+    if (isLoading) return <LoadingAnimation />;
+    if (error) return <p className="error-message">{error}</p>;
+  };
 
-        return(
-            <div className="comment-vote-container"> 
-                <button className="comment-vote-button" onClick={() => handleVote(1)}><img src={upvoteButton} alt="Upvote Arrow" className="vote-icon" /></button>
-                <p className="vote-count">{votes}</p>
-                <button className="comment-vote-button" onClick={() => handleVote(-1)}><img src={downvoteButton} alt="Downvote Arrow" className="vote-icon" /></button>
-            </div>
-        )
+  return (
+    <div className="comment-vote-container">
+      <button className="comment-vote-button" onClick={() => handleVote(1)}>
+        <TiArrowUpOutline className="vote-icon" />
+      </button>
+      <p className="vote-count">{votes}</p>
+      <button className="comment-vote-button" onClick={() => handleVote(-1)}>
+        <TiArrowDownOutline className="vote-icon" />
+      </button>
+    </div>
+  );
 }
 
-export default CommentVotesButton
+export default CommentVotesButton;
