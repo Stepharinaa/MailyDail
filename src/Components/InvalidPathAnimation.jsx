88fb7f5880461b817dc5b1d7a39400f4a9752e35
Stepharinaa <stephanielplee@gmail.com>
2025-04-11 08:15:21 +0100

    adjust sizing for invalid page animation to ensure it is suitable for several media queries

--- src/Components/InvalidPathAnimation.jsx
+++ src/Components/InvalidPathAnimation.jsx
@@ -1,10 +1,14 @@
-import Lottie from "lottie-react"
+import Lottie from "lottie-react";
 import PageNotFoundAnimation from "../assets/page-not-found-animation.json";
 
 function InvalidPathAnimation() {
   return (
-    <div id="invalid-path-loading-container">
-      <Lottie animationData={PageNotFoundAnimation} loop={true} autoplay={true}/>
+    <div>
+      <Lottie
+        animationData={PageNotFoundAnimation}
+        loop={true}
+        autoplay={true}
+      />
     </div>
   );
 }
--- src/Pages/InvalidPathPage.jsx
+++ src/Pages/InvalidPathPage.jsx
@@ -5,6 +5,7 @@ function InvalidPathPage() {
     <div id="invalid-path-animation">
       <h1>404 - Page Not Found!</h1>
       <InvalidPathAnimation />
+      <p>We couldn't find the page you were looking for.</p>
     </div>
   );
 }
