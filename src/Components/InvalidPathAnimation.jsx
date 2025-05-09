2de6315005fb1dc227402932a8b8e7b8ddb8c090
stepharinaa <stephanielplee@gmail.com>
2025-05-09 15:17:48 +0100

    utilise lazy loading for invalid path animation

--- src/Components/InvalidPathAnimation.jsx
+++ src/Components/InvalidPathAnimation.jsx
@@ -1,14 +1,18 @@
-import Lottie from "lottie-react";
+import React, { Suspense, lazy } from "react";
 import PageNotFoundAnimation from "../assets/page-not-found-animation.json";
+
+const Lottie = lazy(() => import("lottie-react"));
 
 function InvalidPathAnimation() {
   return (
     <div>
+      <Suspense fallback={<div>Loading animation...</div>}>
       <Lottie
         animationData={PageNotFoundAnimation}
         loop={true}
         autoplay={true}
       />
+      </Suspense>
     </div>
   );
 }
